8d576d344b1a1c4a7c454922bfdb8610bf93662a
Arockia Jeba Santhiya <arockiyajebasanthiya@gmail.com>
2024-02-11 12:11:25 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { GridComponent } from './grid/grid.component';
-
+import { GridComponent } from '@syncfusion/ej2-angular-grids';
+import { Grid } from '@syncfusion/ej2-angular-grids';
 export const routes: Routes = [
     
 ];
